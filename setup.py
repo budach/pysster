e7a9a229e3dadd9c1a39b8e0e25e1f4af918f1b0
budach <budach@molgen.mpg.de>
2017-12-02 16:30:45 +0100

    up to 1.0.1
    Former-commit-id: 6db3439061fdbd038f875397d7df6ccf9a544996 [formerly 5801919218a487dd5e459bcf6d503cc6e68af6df]
    Former-commit-id: 62ad93db56d0ee1334167243317e3abfa9d448f1
    Former-commit-id: 19399fe3012463f34e118f5fd547de6bc1140304
    Former-commit-id: 544529284878e528feb444a8cc846b76b448391c

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pysster',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'A DNA/RNA Sequence/STructure ClassifiER',
     url = 'https://github.com/budach/pysster',
     author = 'Stefan Budach',
